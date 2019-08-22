cef5ce27fed6ba2b7cebba04a772b20e6dcfbd84
Donovan.Xu <d.east6@gmail.com>
2019-08-22 16:45:28 +0800

    与后端交互

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,12 +2,17 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Home Page</div>
-
-                    <div class="card-body">
-                        I'm an example component.
+                <div class="my-3 p-3 bg-white rounded box-shadow">
+                    <h6 class="border-bottom border-gray pb-2 mb-0">文章列表页</h6>
+                    <div v-for="post in posts" :key="post.id"  class="media text-muted pt-3">
+                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                            <strong class="d-block text-gray-dark">
+                                <router-link :to="{ name: 'posts', params: {id: post.id} }">{{ post.title }}</router-link>
+                            </strong>
+                            {{ post.body }}
+                        </p>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -16,7 +21,15 @@
 
 <script>
     export default {
+        data(){
+            return {
+                posts:[]
+            }
+        },
         mounted() {
+            axios.get('/api/posts').then(response => {
+                this.posts = response.data.data
+            })
             console.log('Component mounted.')
         }
     }
---
+++ resources/js/components/Post.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><h4>{{ post.title }}</h4></div>
+                    <div class="card-body">
+                        {{ post.body }}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Post",
+        data(){
+            return {
+                post: []
+            }
+        },
+        mounted() {
+            axios.get('/api/posts/' + this.$route.params.id).then(response=>{
+                this.post = response.data
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
