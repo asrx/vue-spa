1e6caedcca5b46cafd42c0fdecfc70289a8c5b26
Donovan.Xu <d.east6@gmail.com>
2019-08-23 13:47:16 +0800

    表单验证

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,12 +5,15 @@ window.Vue = require('vue');
 import VueRouter from 'vue-router'
 import router from './routes'
 import App from './components/App'
+import VeeValidate from 'vee-validate'
 
 Vue.use(VueRouter)
+Vue.use(VeeValidate)
+// Vue.component('ValidationProvider', VeeValidate);
 // 全局
 Vue.component('app',App)
 
 new Vue({
     el: '#app',
-    router // router : router
+    router
 });
--- resources/js/components/register/RegisterForm.vue
+++ resources/js/components/register/RegisterForm.vue
@@ -3,7 +3,11 @@
         <div class="form-group row">
             <label for="name" class="col-md-4 col-form-label text-md-right">用户名</label>
             <div class="col-md-6">
-                <input id="name" v-model="name" type="text" class="form-control" name="name" required autocomplete="name" autofocus>
+                <input id="name"
+                       :class="{ 'is-invalid' : errors.has('name') }"
+                       v-validate="{required: true, min: 4}"
+                       v-model="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
+                <span class="form-check-label" v-show="errors.has('name')">{{errors.first('name')}}</span>
             </div>
         </div>
 
@@ -11,23 +15,37 @@
             <label for="email" class="col-md-4 col-form-label text-md-right">邮箱</label>
 
             <div class="col-md-6">
-                <input id="email" v-model="email" type="email" class="form-control" name="email" required autocomplete="email">
+                <input id="email"
+                       :class="{ 'is-invalid' : errors.has('email') }"
+                       v-validate="{required: true, min: 4}"
+                       v-model="email" type="email" class="form-control" name="email" autocomplete="email">
+
+                <span class="form-check-label" v-show="errors.has('email')">{{errors.first('email')}}</span>
             </div>
         </div>
 
-        <div class="form-group row">
+        <div class="form-group row" :class="{ 'is-invalid' : errors.has('password') }">
             <label for="password" class="col-md-4 col-form-label text-md-right">密码</label>
 
             <div class="col-md-6">
-                <input id="password" v-model="password" type="password" class="form-control" name="password" required autocomplete="new-password">
+                <input id="password"
+                       :class="{ 'is-invalid' : errors.has('password') }"
+                       v-validate="{ required: true, min: 6 }"
+                       v-model="password" type="password" class="form-control" name="password" autocomplete="new-password">
+                <span class="form-check-label" v-show="errors.has('password')">{{errors.first('password')}}</span>
             </div>
         </div>
 
-        <div class="form-group row">
+        <div class="form-group row" :class="{ 'is-invalid' : errors.has('password_confirmation') }">
             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">确认密码</label>
 
             <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                <input id="password-confirm"
+                       :class="{ 'is-invalid' : errors.has('password_confirmation') }"
+                       v-validate="{required: true, min: 6, confirmed: password}"
+                       type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
+
+                <span class="form-check-label" v-show="errors.has('password_confirmation')">{{errors.first('password_confirmation')}}</span>
             </div>
         </div>
 
